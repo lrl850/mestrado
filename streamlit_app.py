d07926972b675397c8c92e600e6b386c38a40894
lrl850 <lrl850.si@gmail.com>
2025-04-05 21:25:14 -0300

    k

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
+import plotly.express as px 
 
 # Configuração da página
 st.set_page_config(page_title="Navegação com Menu", layout="wide")
